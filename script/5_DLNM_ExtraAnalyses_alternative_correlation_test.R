420775d36488f22058cc84d2666a0b55aca4e662
radiciandrea <andrea.radici93@libero.it>
2026-05-28 18:14:38 +0200

    updated with new indicators and new analysis with predicted vs observed

--- script/5_DLNM_ExtraAnalyses_alternative_correlation_test.R
+++ script/5_DLNM_ExtraAnalyses_alternative_correlation_test.R
@@ -4,18 +4,20 @@ library(dplyr)
 library(ggplot2)
 
 # Load objects ----
-bio.matrix_sel               <- readRDS("data/bio.matrix_cal.rds") %>%
-  select(c("ID", "year", "week", "medianTweek", "cumPrecweek", "eggs"))
-bio.matrix_test_regions_pred <- readRDS("data/bio.matrix_test_regions_pred.rds") %>%
-  select(c("ID", "year", "week", "medianTweek", "cumPrecweek", "eggs"))
-bio.matrix_test_years        <- readRDS("data/bio.matrix_test_years.rds") %>%
-  select(c("ID", "year", "week", "medianTweek", "cumPrecweek", "eggs"))
+bio.matrix_sel               <- readRDS("data/bio.matrix_sel_pred.rds") %>%
+  select(c("ID", "year", "week", "medianTweek", "cumPrecweek", "eggs", "pred"))
+bio.matrix_test_regions <- readRDS("data/bio.matrix_test_regions_pred.rds") %>%
+  select(c("ID", "year", "week", "medianTweek", "cumPrecweek", "eggs", "pred"))
+bio.matrix_test_years        <- readRDS("data/bio.matrix_test_years_pred.rds") %>%
+  select(c("ID", "year", "week", "medianTweek", "cumPrecweek", "eggs", "pred"))
 
 # there are duplicates in bio.matrix_test_regions_pred (ID 981, y 2019)
 bio.matrix_test_regions_pred <- bio.matrix_test_regions_pred %>%
   filter(ID != 981)
 
-bio.matrix <- rbind(bio.matrix_sel, bio.matrix_test_regions_pred, bio.matrix_test_years)
+bio.matrix <- rbind(bio.matrix_sel, bio.matrix_test_regions, bio.matrix_test_years)
+
+# with temperature treshold only ----
 
 predicted_cross_sel <- bio.matrix  %>%
   group_by(ID) %>% #group_by(ID, year) %>%
@@ -48,8 +50,13 @@ cor_result <- cor.test(observed_predicted_cross$threshold_eggs,
                        observed_predicted_cross$threshold_thermal,
                        method = "pearson")
 
+rmse = sqrt(mean((observed_predicted_cross$threshold_thermal - observed_predicted_cross$threshold_eggs )^2))
+mae = mean(abs(observed_predicted_cross$threshold_thermal - observed_predicted_cross$threshold_eggs ))
+
 cat("Pearson r (onset vs threshold crossing):", round(cor_result$estimate, 3), "\n")
 cat("p-value:                                ", format(cor_result$p.value, scientific = TRUE), "\n")
+cat("rmse:                                ", format(rmse, 3), "\n")
+cat("mae:                                ", format(mae, 3), "\n")
 
 ggplot(observed_predicted_cross,
              aes(x = threshold_thermal, y = threshold_eggs)) +
@@ -61,44 +68,81 @@ ggplot(observed_predicted_cross,
   theme_minimal(base_size = 11) +
   theme(panel.grid.minor = element_blank())
 
-ggsave( "outputs/Fig_threshold_vs_observed_onset.png", width = 7, height = 6, dpi = 300)
+ggsave( "outputs/Fig_observed_onset_vs_thermal_threshold_onset.png", width = 7, height = 6, dpi = 300)
 
-### test esempio
-r = 1
-observed_predicted_cross[r,]
-plot(bio.matrix %>% filter(year == observed_predicted_cross$year[r],
-                               ID == observed_predicted_cross$ID[r]) %>% pull(eggs))
+# ### test esempio
+# r = 1
+# observed_predicted_cross[r,]
+# plot(bio.matrix %>% filter(year == observed_predicted_cross$year[r],
+#                                ID == observed_predicted_cross$ID[r]) %>% pull(eggs))
 
 
-
-# with precipitation ----
+# only predicted (with the whole model) vs observed ----
 
 predicted_cross_sel <- bio.matrix  %>%
   group_by(ID) %>% #group_by(ID, year) %>%
-  summarise(threshold_rain = week[which(cumPrecweek > 12)[1]]) %>%
-  filter(!is.na(threshold_rain)) %>%
+  summarise(model_predicted = week[which(pred > 10)[1]]) %>%
+  filter(!is.na(model_predicted)) %>%
   ungroup()
 
 observed_predicted_cross = left_join(observed_cross_sel, predicted_cross_sel, by = c("ID")) %>%
-  filter(!is.na(threshold_rain))
+  filter(!is.na(model_predicted))
 
 #plot e corr
 
-cor_result <- cor.test(observed_predicted_cross$threshold_eggs,
-                       observed_predicted_cross$threshold_rain,
+cor_result <- cor.test(observed_predicted_cross$model_predicted,
+                       observed_predicted_cross$threshold_eggs,
                        method = "pearson")
 
-# significativa e negativa, pensa te
+rmse = sqrt(mean((observed_predicted_cross$model_predicted - observed_predicted_cross$threshold_eggs )^2))
+mae = mean(abs(observed_predicted_cross$model_predicted - observed_predicted_cross$threshold_eggs ))
 
 cat("Pearson r (onset vs threshold crossing):", round(cor_result$estimate, 3), "\n")
 cat("p-value:                                ", format(cor_result$p.value, scientific = TRUE), "\n")
+cat("rmse:                                ", format(rmse, 3), "\n")
+cat("mae:                                ", format(mae, 3), "\n")
 
 ggplot(observed_predicted_cross,
-       aes(x = threshold_rain, y = threshold_eggs)) +
+       aes(x = model_predicted, y = threshold_eggs)) +
   geom_point(alpha = 0.2, size = 1, col = "orange") + #
   geom_smooth(method = "lm", col = "firebrick", linewidth = 1) +
-  labs(x     = "Week of 17.5°C threshold crossing",
+  labs(x     = "Predicted season onset (week)",
        y     = "Observed season onset (week)",
-       title = "rain threshold vs observed season onset") +
+       title = "predicted vs observed season onset") +
   theme_minimal(base_size = 11) +
   theme(panel.grid.minor = element_blank())
+
+
+ggsave( "outputs/Fig_observed_onset_vs_modelled_onset.png", width = 7, height = 6, dpi = 300)
+
+# # with precipitation only ----
+# 
+# predicted_cross_sel <- bio.matrix  %>%
+#   group_by(ID) %>% #group_by(ID, year) %>%
+#   summarise(threshold_rain = week[which(cumPrecweek > 12)[1]]) %>%
+#   filter(!is.na(threshold_rain)) %>%
+#   ungroup()
+# 
+# observed_predicted_cross = left_join(observed_cross_sel, predicted_cross_sel, by = c("ID")) %>%
+#   filter(!is.na(threshold_rain))
+# 
+# #plot e corr
+# 
+# cor_result <- cor.test(observed_predicted_cross$threshold_eggs,
+#                        observed_predicted_cross$threshold_rain,
+#                        method = "pearson")
+# 
+# # significativa e negativa, pensa te
+# 
+# cat("Pearson r (onset vs threshold crossing):", round(cor_result$estimate, 3), "\n")
+# cat("p-value:                                ", format(cor_result$p.value, scientific = TRUE), "\n")
+# 
+# ggplot(observed_predicted_cross,
+#        aes(x = threshold_rain, y = threshold_eggs)) +
+#   geom_point(alpha = 0.2, size = 1, col = "orange") + #
+#   geom_smooth(method = "lm", col = "firebrick", linewidth = 1) +
+#   labs(x     = "Week of 17.5°C threshold crossing",
+#        y     = "Observed season onset (week)",
+#        title = "rain threshold vs observed season onset") +
+#   theme_minimal(base_size = 11) +
+#   theme(panel.grid.minor = element_blank())
